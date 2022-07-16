6cd2ad7a9ab5a06b47dc0444e8b68f94b05d685c
klassiq1 <hardeyfehmie24@gmail.com>
2022-07-16 04:30:50 -0700

    modify

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    return (0);
+
+}
+
+
