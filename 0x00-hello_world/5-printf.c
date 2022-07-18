c4e7e60b9083709db2d9438e7ad45f7ec8d8ecb2
klassiq1 <hardeyfehmie24@gmail.com>
2022-07-18 02:23:53 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * this decription is for printf function
  * returns 0
  */
-int main(void)
+int main(void)	//decription for the main
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
