02290b95611e3c7b4dea11afe50577f1df9381ae
klassiq1 <hardeyfehmie24@gmail.com>
2022-07-17 18:09:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,14 @@
 #include<stdio.h>
 
-/**
- *
- *  * main - Entry point
- *
- *   * Description: prints \"Programming is like building a multilingual puzzle\"
- *
- *    * Return: 0
- *
- *     */
 
+/**
+ *  * main - Entry point
+ *   * Description: prints \"Programming is like building a multilingual puzzle\"
+ *    * Return: 0
+ *     */
 int main(void) 
 {
-	char str1[] = "Programming is like building a multilingual puzzle\n";
+	char str1[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(str1);
 
 	return (0);
