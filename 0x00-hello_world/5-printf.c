525e273c586cf8fb22ded2c0fe52c6fa1d5b1d8f
adefemi <hardeyfehmie24@gmail.com>
2022-09-08 13:49:33 -0700

    ft

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * this decription is for printf function
  * returns 0
  */
-int main(void)
+int main(void) /*prints text*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
