bc43a5036196680996fe8fc8cf1afaa12ddf45a0
adefemi <hardeyfehmie24@gmail.com>
2022-09-08 17:22:40 -0700

    hj

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-/*
+
+/**
  *
- * main - Entry point
+ * *main - Entry point
  *
- *Descripton - prints "with proper grammar, but the outcome is a piece of art,"
+ * *Descripton - prints "with proper grammar, but the outcome is a piece of art"
  *
- * Return: Always 0 (Success)
+ * *Return: Always 0 (Success)
  *
  */
+
 int main(void)
 
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");	
+
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 
 }
