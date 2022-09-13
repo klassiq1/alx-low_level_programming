ab76217572fcdb07f8ba694c6918205c6aedbb20
adefemi <hardeyfehmie24@gmail.com>
2022-09-13 07:46:10 -0700

    clueless

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hello.h"
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	print_alphabet();
+	return (0);
+}
