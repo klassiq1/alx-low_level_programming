4e615017497af70d75b46449166de2b1a8a14100
adefemi <hardeyfehmie24@gmail.com>
2022-09-13 21:17:49 -0700

    fgd

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /*
  * add - Adds two integers and returns
-  *add : add two numbers and return the value
-  *  * the result
- * * @a: number one.
-  *
-  *  * @b: number two.
- *   * Return: Add of number one and number two.
+ * add : add two numbers and return the value
+ * the result
+ * @a: number one.
+ * @b: number two.
+ * Return: Add of number one and number two.
  */
 
 int add(int a, int b)
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,10 +17,10 @@ int main(void)
 	{
 		if (boolean)
 		{
-			fn = n1 + n2
+			fn = n1 + n2;
 			printf(", %ld", fn);
 			n1 = n2;
-			n2 = fn
+			n2 = fn;
 		}
 		else
 		{
